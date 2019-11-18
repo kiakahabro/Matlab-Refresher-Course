cefa8266d418ab4ad46988d95ab264d162da66e8
Tim Farnworth <tkfarnworth@gmail.com>
2019-11-18 18:06:15 +1100

    Remove testfeedback

--- testfeedback/assumesPassed.m
+++
@@ -1,7 +0,0 @@
-function assumesPassed(testCase,varargin)
-%ASSUMESPASSED Summary of this function goes here
-%   Detailed explanation goes here
-	[ares, astr]= assumes(varargin{:});
-	assumeTrue(testCase, ares, astr);
-end
-
