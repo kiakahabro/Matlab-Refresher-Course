f9fc68900d409d7373288f3080ff4f5cf8381876
Tim Farnworth <tkfarnworth@gmail.com>
2019-11-18 19:01:20 +1100

    Resolve

---
+++ testfeedback/giveFeedback.m
@@ -0,0 +1,6 @@
+function [outputArg1,outputArg2] = giveFeedback(testResults)
+%GIVEFEEDBACK Summary of this function goes here
+%   Detailed explanation goes here
+	[resultsStr, npassed, ntests]  = testToString(testResults);
+end
+
