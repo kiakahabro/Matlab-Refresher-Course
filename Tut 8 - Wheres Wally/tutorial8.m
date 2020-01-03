50b4d7309bdefa52dffbf5f51f8b4920d641d964
Nic O'Dell <nicodell88@gmail.com>
2020-01-03 14:43:38 +1100

    Tutorial 8 template

---
+++ Tut 8 - Wheres Wally/tutorial8.m
@@ -0,0 +1,30 @@
+%% TODO: Copy the completed imageFilter, zncc, and createCircle into this directory.
+%% TODO: Clear the console and workspace.
+
+%% TODO: Set the default.
+% axesfontsize
+% axeslabelfontsize
+% axes title fontsize
+%% TODO: Read input image: "wheres-wally.png" from file.
+
+%% TODO: Read the template from file.
+
+%% TODO: In Figure(1) plot a 1x2 subplot showing
+% The input image
+% The template
+
+% TIP: Don't forget the axis labels and title.
+% TIP: Turn "axis on" to display the pixel coordinates.
+%% TODO: Perform template matching.
+
+%% TODO: Plot the resulting similarity measure in Figure(2) using imagesc.
+% TIP: Don't forget the axis labels and title.
+% TIP: Use a colorbar!
+
+%% TODO: Plot circles around the 3 most likely Wally locations in Figure(3).
+
+%% TODO: Create mug-shots. Create a 1x3 subplot in Figure(4).
+% Hint: use a for loop to avoid copying and pasting code.
+
+%% TODO: Export Figure 1, 2, 3 and 4 as "png" using saveas.
+
